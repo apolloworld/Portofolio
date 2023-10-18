25e9f3a020a5e590cb9696e58a50b507492b1213
Joseph Zhang <>
2023-10-18 02:06:27 -0500

    Change nae

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Denis Varennikov</h3>
+          <h3>Designed and Developed by Denys Tverdokhlib</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
